efe715c383ac47146305c0246aab3d02e916b2de
igalekseev <igor.alekseev@itep.ru>
2014-10-14 21:54:02 +0400

    init.sh

--- init.sh
+++ init.sh
@@ -2,8 +2,15 @@
 ../cpldtool/cpldtool 1 p main.bin
 if [ $? == 0 ] ; then
     echo Initializing ...
-    sleep 2
+    sleep 1
+    echo Sleeping...
+    sleep 1
+    echo Sleeping...
+    sleep 1
+    echo Sleeping...
+    sleep 1
     ./progcsr.sh
+    sleep 1
     ./clockenb.sh
 else
     ../cpldtool/cpldtool 1 p
